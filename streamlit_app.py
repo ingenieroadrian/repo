79a1a1fddabcc5c4ed9b463a502b2c8e59efee46
Adrian <76918152+ingenieroadrian@users.noreply.github.com>
2023-08-07 18:31:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,27 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# Import the ATP match data for the 2021-2022 season
-df = pd.read_csv("atp_matches_2021_2022.csv")
+# Load the data
+df = pd.read_csv("data.csv")
 
-# Select the players from the top 10 positions on ATP
-top_10_players = df["winner_name"].value_counts()[:10].index
+# Create a bar chart of the number of titles won by each player
+plt.bar(df["Player"], df["Titles"])
+plt.xlabel("Player")
+plt.ylabel("Titles")
+plt.title("Number of Titles Won by Each Player in 2022")
+plt.show()
 
-# Create a heat map for each player
-for player in top_10_players:
-    # Get the player's match data
-    player_df = df[df["winner_name"] == player]
+# Create a line chart of the player rankings over the course of the season
+plt.plot(df["Date"], df["Ranking"])
+plt.xlabel("Date")
+plt.ylabel("Ranking")
+plt.title("Player Rankings Over the Course of the 2022 Season")
+plt.show()
 
-    # Create a heat map of the player's wins and losses by surface
-    plt.imshow(player_df["surface"].value_counts().to_numpy(), cmap="hot")
-    plt.title(player)
-    plt.show()
+# Create a scatter plot of the player's service and return percentages
+plt.scatter(df["Service Percentage"], df["Return Percentage"])
+plt.xlabel("Service Percentage")
+plt.ylabel("Return Percentage")
+plt.title("Player's Service and Return Percentages in 2022")
+plt.show()
 
